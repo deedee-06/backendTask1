3103d6224ff61e99a83ae7dc8e4e5504013b310e
deedee-06 <ekezedivinefavour@gmail.com>
2024-04-26 15:29:42 +0100

    This is my first push commit in

---
+++ variable.js
@@ -0,0 +1,14 @@
+
+
+var myName="Deedee";
+var myAge= 2;
+
+console.log(`my name is ${myName}, i am ${myAge} years old`);
+
+// function  add (a,b){
+//     console.log (a + b);
+// }
+// add(5,4);
+
+var add =(a,b) => console.log(a + b);
+add (5, 2)
