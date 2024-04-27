c0de710e13b3f0743e8fb078a1f9e6c6c43cba1e
deedee-06 <ekezedivinefavour@gmail.com>
2024-04-28 00:20:40 +0100

    updated http methods

--- index.js
+++ index.js
@@ -4,6 +4,28 @@ const app = express();
 
 const port = 3000;
 
+app.get("/deedee", (req, res) => {
+  res.send(
+    "Hi! This is Deedee. The most recognized female developer. This is a get request"
+  );
+});
+
+app.post("/deedee", (req, res) => {
+  res.send("Hi, there. This is a post request");
+});
+
+app.patch("/deedee", (req, res) => {
+  res.send("Hello senior dev! This is a patch request");
+});
+
+app.put("/deedee", (req, res) => {
+  res.send("Hello senior dev! This is a put request");
+});
+
+app.delete("/deedee", (req, res) => {
+  res.send("Hello senior dev! This is a delete request");
+});
+
 app.listen(port, () => {
   console.log("server is running");
 });
